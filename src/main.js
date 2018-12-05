714704a0f7174049c22c7805cc151b1226ccd1ea
Patrick Kuckelsberg <patrick.kuckelsberg@ise.de>
2018-12-05 12:34:33 +0100

    config changes

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee, faOilCan, faCogs, faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCoffee, faOilCan, faCogs, faHome
-  );
+library.add(faCoffee, faOilCan, faCogs, faHome);
 
 Vue.use(BootstrapVue);
 
